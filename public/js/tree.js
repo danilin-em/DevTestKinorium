4072ecba4b0504e6f3293561d74eb250cccf7210
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-03-30 12:53:48 +0400

    rebase #5 examples

---
+++ public/js/tree.js
@@ -0,0 +1,12 @@
+const Tree = function (el) {
+    const li_ul = el.querySelectorAll('li>ul');
+    for (const x of li_ul) {
+        x.style.display = 'none';
+        x.parentNode.addEventListener('mouseenter', () => {
+            x.style.display = 'block';
+        });
+        x.parentNode.addEventListener('mouseleave', () => {
+            x.style.display = 'none';
+        });
+    }
+};
